600366ae721f2625feab46ccb7f742e80fe342ee
sayandcode <sayandcode@gmail.com>
2022-09-27 23:37:29 +0530

    Initialize eslint and tailwind(with custom fonts)
    The unnecessary files were deleted, and custom theme was created in
    tailwind config.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      'visby-400': ['VisbyCF400', 'sans-serif'],
+      'visby-500': ['VisbyCF500', 'sans-serif'],
+      'visby-600': ['VisbyCF600', 'sans-serif'],
+      'visby-700': ['VisbyCF700', 'sans-serif'],
+      'zuume-700': ['Zuume700', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        ipxRed: '#F61354',
+      },
+    },
+  },
+  plugins: [],
+};
